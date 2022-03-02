579d017f2b1848bb1596bbf227354d095477db11
luisedu.unifal <luisedu.unifal@gmail.com>
2022-03-02 09:43:02 -0300

    Consumer rabbit

--- email/src/main/java/com/microservice/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/microservice/email/consumers/EmailConsumer.java
@@ -1,7 +1,25 @@
 package com.microservice.email.consumers;
 
+import com.microservice.email.dtos.EmailDTO;
+import com.microservice.email.models.EmailModel;
+import com.microservice.email.services.EmailService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
+
+    @Autowired
+    EmailService emailService;
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue}")
+    public void listen(@Payload EmailDTO emailDTO) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDTO, emailModel);
+        emailService.sendEmail(emailModel);
+        System.out.println("Email Status: " + emailModel.getStatusEmail().toString());
+    }
 }
